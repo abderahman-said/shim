35d20eb1569e3829562d561dc4aeb4f6c0c3367e
abdo rady <109882308+abderahman-said@users.noreply.github.com>
2023-08-22 14:39:09 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,114 @@
+AOS.init({
+  duration: 800
+});
+
+function progressBarScroll() {
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop,
+      height = document.documentElement.scrollHeight - document.documentElement.clientHeight,
+      scrolled = (winScroll / height) * 100;
+  document.getElementById("progressBar").style.width = scrolled + "%";
+}
+
+var section = document.querySelector('.relative_wats');
+var scrollThreshold = 500;
+
+window.addEventListener('scroll', function() {
+  if (window.pageYOffset > scrollThreshold) {
+    section.style.display = 'block';
+  } else {
+    section.style.display = 'none';
+  }
+});
+window.onscroll = function () {
+  progressBarScroll();
+};
+
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    rtl:true,
+      navText : ['<i class="fa-solid fa-arrow-right" aria-hidden="true"></i>','<i class="fa-solid fa-arrow-left" aria-hidden="true"></i>'],
+    responsiveClass:true,
+    nav:true,
+    responsive:{
+      0:{
+          items:1
+      },
+      500:{
+        items:1
+    },
+      600:{
+          items:1
+      },
+      800:{
+        items:2
+    },
+      1000:{
+          items:3
+      }
+    }
+})
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
+
+$(document).ready(preloderFunction());
+    
+function preloderFunction() {
+  
+    setTimeout(function() {
+        document.getElementById("page-top").scrollIntoView();
+        
+        $('#ctn-preloader').addClass('loaded');  
+        $('body').removeClass('no-scroll-y');
+
+        if ($('#ctn-preloader').hasClass('loaded')) {
+            $('#preloader').delay(500).queue(function() {
+                $(this).remove();
+                
+                afterLoad();
+                
+            });
+        }
+    }, 700);
+}
+
+
+
+function afterLoad() {
+}
+const contactBtn = document.querySelector('.contact_btn');
+
+contactBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: document.body.scrollHeight,
+    behavior: 'smooth'
+  });
+});
+
+
+const contactBtn2 = document.querySelector('.header_btn2');
+
+contactBtn2.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
+
+
+
